1dec86120365fd5c7c4957ca49844c417208a1ce
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-01-09 15:33:04 +0700

    init projects

---
+++ spring-overview/src/main/java/com/seyha/springboot/demo/springoverview/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.seyha.springboot.demo.springoverview.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "<h1>Hello world<h1/>";
+    }
+
+    @GetMapping("/get")
+    public String sage() {
+        String text = "Hello"
+                +" World";
+        return "<h1>"+ text+ "<h1/>";
+    }
+}

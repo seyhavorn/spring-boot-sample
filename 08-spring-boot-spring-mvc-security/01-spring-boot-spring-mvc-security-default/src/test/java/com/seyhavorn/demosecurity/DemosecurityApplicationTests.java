91f08cf837c898a428f5e50a0b3104990f466475
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-01-19 16:42:14 +0700

    Update:
    - Spring boot MVC Security default

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/seyhavorn/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyhavorn.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
